fa61bef3bfe30591010656c6b790b8e1d1e33604
minillinim <michael.imelfort@gmail.com>
2017-01-19 22:03:58 +1000

    jkdfhjdsf

--- tatt/layout.py
+++ tatt/layout.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 from matplotlib.collections import PatchCollection
 import matplotlib
+import matplotlib.pyplot as plt
 
 from star import Star
 from attrs import Point
@@ -25,6 +26,9 @@ class Layout(object):
                 self._update_limits(xs, ys)
                 ax.plot( xs, ys, '-', color=star.color, linewidth=star.line_width)
 
+        plt.rcParams['axes.facecolor']='black'
+        plt.rcParams['savefig.facecolor']='black'
+
         ax.set_xlim([self.limits["xmin"] - 1, self.limits["xmax"] + 1])
         ax.set_ylim([self.limits["ymin"] - 1, self.limits["ymax"] + 1])
 
@@ -40,12 +44,18 @@ class Layout(object):
         if ymax > self.limits["ymax"]: self.limits["ymax"] = ymax
 
     def _load_stars(self, config):
+
+
+        from random import randint
+        r = lambda: randint(0,255)
+        choose = lambda: '#%02X%02X%02X' % (r(),r(),r())
+
         self.stars = []
 
         dupe_width_mod = 3.
         dupe_color = "#aaaaaa"
         dupe_rotate = 3.
-        decay = 5.
+        decay = 3.
 
         real_star_indicies = []
 
@@ -57,7 +67,7 @@ class Layout(object):
                 position=data.position,
                 rotation=float(data.rotation),
                 tees=data.tees,
-                color=data.color,
+                color="#555555",#choose(),#data.color,
                 orientation=data.orientation,
                 line_width=float(data.line_width),
                 scaler=float(data.scaler),
@@ -65,28 +75,30 @@ class Layout(object):
             star.initialise()
  
             sd = star.duplicate()
-            sd.color = dupe_color
+            sd.color = "#555555"#dupe_color
             sd.rotation = star.rotation + dupe_rotate / 180. * np.pi
             sd.line_width = star.line_width * dupe_width_mod
             sd.initialise()
 
-            self.stars.append(sd)
+            #self.stars.append(sd)
             self.stars.append(star)
             real_star_indicies.append(len(self.stars)-1)
+            break
 
         for rsi in real_star_indicies:
             for c in range(1, 12):
                 star = self.stars[rsi].duplicate()
+                star.color="#555555"#Schoose()
                 star.rotation = float(c * np.pi / 7.) 
                 star.outer_radius = star.outer_radius + (float(c) * 1./decay)
                 star.position = Point(star.position.x + (c * 10.), star.position.y)
                 star.initialise()
                 
                 sd = star.duplicate()
-                sd.color = dupe_color
+                sd.color = "#555555"#Sdupe_color
                 sd.rotation = star.rotation + dupe_rotate / 180. * np.pi
                 sd.line_width = star.line_width * dupe_width_mod
                 sd.initialise()
 
-                self.stars.append(sd)
+                #self.stars.append(sd)
                 self.stars.append(star)
